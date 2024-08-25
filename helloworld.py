1f662bfd7369f14ce83264ec5fa596ce20d46c5c
wunmyte100 <wunmyte100@gmail.com>
2024-08-25 17:22:44 +0100

    Learning from w3

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,55 @@
 print ("Hello, World!")
+if 5>2:
+    print ("Five is greater than two!")
+#This is a comment
+print("Hello World!")
+#print("Hello, World!")
+print("cheers, mate!")
+
+x=5
+y="John"
+print (x)
+print (y)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)
+print(x)
+print(y)
+print(z)
+
+x = str(3)    # x will be '3'
+y = int(3)
+print(type(x))
+print(type(y))
+a=4
+A="Sally"
+print(A)
+print(a)
+x,y,z="Orange", "Book", "House"
+print(x,y,z)
+x=y=z="Orange"
+print(y)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(z)
+print(y)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+print(x + y + z)
+x = 5
+y = 10
+print(x + y)
+
+x = 5
+y = "John"
+print(x,y)
+
+x = "awesome"
+print("Python is " + x)
+myfunc()
---
+++ new-python.py
@@ -0,0 +1,75 @@
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+  x = "interesting"
+  print("Python is " + x)
+
+myfunc()
+print("Python is " + x)
+x=5
+print(type(x))
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+import random
+print(random.randrange(1, 10))
+
+print("Hello")
+print('Hello')
+print(5)
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+a = """Lorem ipsum dolor sit amet,
+ut labore et dolore magna aliqua."""
+print(a)
+
+a = "Hello, World!"
+print(a[5])
+
+for x in "apple":
+  print(x)
+
+a = "Hello, World!"
+print(len(a))
+
+txt = "The best things in life are free!"
+print("bag" in txt)
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("No, 'free' is present.")
+
+  txt = "The best things in life are free!"
+print("expensive" not in txt)
